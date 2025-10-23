1175e44c94f25ddf6526636aec8008012e2bd728
ZachJHansen <zachansen62@gmail.com>
2025-10-23 10:45:37 -0500

    cleanup before merge

--- src/translating/formula_representation/tau_star_cl.rs
+++ src/translating/formula_representation/tau_star_cl.rs
@@ -1,12 +1,6 @@
 use {
     super::tau_star::TauStar,
-    crate::{
-        syntax_tree::{asp::mini_gringo_cl as asp, fol::sigma_0 as fol},
-        translating::formula_representation::tau_star::{
-            choose_fresh_global_variables, choose_fresh_variable_names,
-        },
-    },
-    indexmap::IndexSet,
+    crate::syntax_tree::{asp::mini_gringo_cl as asp, fol::sigma_0 as fol},
 };
 
 // // |t|
@@ -129,67 +123,69 @@ use {
 // }
 
 // Translate a propositional body literal
-fn tau_b_propositional_literal(l: asp::Literal) -> fol::Formula {
-    let atom = l.atom;
-    match l.sign {
-        asp::Sign::NoSign => fol::Formula::AtomicFormula(fol::AtomicFormula::Atom(fol::Atom {
-            predicate_symbol: atom.predicate_symbol,
+// fn tau_b_propositional_literal(l: asp::Literal) -> fol::Formula {
+//     let atom = l.atom;
+//     match l.sign {
+//         asp::Sign::NoSign => fol::Formula::AtomicFormula(fol::AtomicFormula::Atom(fol::Atom {
+//             predicate_symbol: atom.predicate_symbol,
 
-            terms: vec![],
-        })),
-        asp::Sign::Negation => fol::Formula::UnaryFormula {
-            connective: fol::UnaryConnective::Negation,
-            formula: fol::Formula::AtomicFormula(fol::AtomicFormula::Atom(fol::Atom {
-                predicate_symbol: atom.predicate_symbol,
-                terms: vec![],
-            }))
-            .into(),
-        },
-        asp::Sign::DoubleNegation => fol::Formula::UnaryFormula {
-            connective: fol::UnaryConnective::Negation,
-            formula: fol::Formula::UnaryFormula {
-                connective: fol::UnaryConnective::Negation,
-                formula: fol::Formula::AtomicFormula(fol::AtomicFormula::Atom(fol::Atom {
-                    predicate_symbol: atom.predicate_symbol,
-                    terms: vec![],
-                }))
-                .into(),
-            }
-            .into(),
-        },
-    }
-}
+//             terms: vec![],
+//         })),
+//         asp::Sign::Negation => fol::Formula::UnaryFormula {
+//             connective: fol::UnaryConnective::Negation,
+//             formula: fol::Formula::AtomicFormula(fol::AtomicFormula::Atom(fol::Atom {
+//                 predicate_symbol: atom.predicate_symbol,
+//                 terms: vec![],
+//             }))
+//             .into(),
+//         },
+//         asp::Sign::DoubleNegation => fol::Formula::UnaryFormula {
+//             connective: fol::UnaryConnective::Negation,
+//             formula: fol::Formula::UnaryFormula {
+//                 connective: fol::UnaryConnective::Negation,
+//                 formula: fol::Formula::AtomicFormula(fol::AtomicFormula::Atom(fol::Atom {
+//                     predicate_symbol: atom.predicate_symbol,
+//                     terms: vec![],
+//                 }))
+//                 .into(),
+//             }
+//             .into(),
+//         },
+//     }
+// }
 
 // Translate a rule using a pre-defined list of global variables
-fn tau_star_rule(r: &asp::Rule, globals: &[String]) -> fol::Formula {
-    // match r.head.predicate() {
-    //     Some(_) => {
-    //         todo!()
-    //     }
-    //     // Handles the case when we have a rule with an empty head
-    //     None => fol::Formula::BinaryFormula {
-    //         connective: fol::BinaryConnective::Implication,
-    //         lhs: tau_body(r.body.clone(), r.global_variables()).into(),
-    //         rhs: fol::Formula::AtomicFormula(fol::AtomicFormula::Falsity).into(),
-    //     }
-    //     .universal_closure(),
-    // }
-    todo!()
-}
+// fn tau_star_rule(r: &asp::Rule, globals: &[String]) -> fol::Formula {
+// match r.head.predicate() {
+//     Some(_) => {
+//         todo!()
+//     }
+//     // Handles the case when we have a rule with an empty head
+//     None => fol::Formula::BinaryFormula {
+//         connective: fol::BinaryConnective::Implication,
+//         lhs: tau_body(r.body.clone(), r.global_variables()).into(),
+//         rhs: fol::Formula::AtomicFormula(fol::AtomicFormula::Falsity).into(),
+//     }
+//     .universal_closure(),
+// }
+// todo!()
+//}
 
-fn tau_star(p: asp::Program) -> fol::Theory {
-    let globals = choose_fresh_global_variables(&p);
-    let mut formulas: Vec<fol::Formula> = vec![]; // { forall G V ( val_t(V) & tau^B(Body) -> p(V) ), ... }
-    for r in p.rules.iter() {
-        formulas.push(tau_star_rule(r, &globals));
-    }
-    fol::Theory { formulas }
-}
+// fn tau_star(p: asp::Program) -> fol::Theory {
+//     // let globals = choose_fresh_global_variables(&p);
+//     // let mut formulas: Vec<fol::Formula> = vec![]; // { forall G V ( val_t(V) & tau^B(Body) -> p(V) ), ... }
+//     // for r in p.rules.iter() {
+//     //     formulas.push(tau_star_rule(r, &globals));
+//     // }
+//     // fol::Theory { formulas }
+//     todo!()
+// }
 
 impl TauStar for asp::Program {
     type Output = fol::Theory;
 
     fn tau_star(self) -> Self::Output {
-        tau_star(self)
+        //tau_star(self)
+        todo!()
     }
 }
