84ebb13bdef7023f2713cce3d374fb385271ff36
ZachJHansen <zachansen62@gmail.com>
2026-05-19 09:09:37 -0500

    removing integer-division and integer-modulo

--- src/formatting/asp/mini_gringo_cl/default.rs
+++ src/formatting/asp/mini_gringo_cl/default.rs
@@ -48,9 +48,7 @@ impl Display for Format<'_, BinaryOperator> {
             BinaryOperator::Subtract => write!(f, "-"),
             BinaryOperator::Multiply => write!(f, "*"),
             BinaryOperator::Divide => write!(f, "/"),
-            BinaryOperator::DivideInteger => write!(f, "//"),
             BinaryOperator::Modulo => write!(f, "\\"),
-            BinaryOperator::ModuloInteger => write!(f, "@"),
             BinaryOperator::Interval => write!(f, ".."),
         }
     }
@@ -62,12 +60,7 @@ impl Precedence for Format<'_, Term> {
             Term::PrecomputedTerm(PrecomputedTerm::Numeral(1..)) => 1,
             Term::UnaryOperation { .. } | Term::PrecomputedTerm(_) | Term::Variable(_) => 0,
             Term::BinaryOperation {
-                op:
-                    BinaryOperator::Multiply
-                    | BinaryOperator::Divide
-                    | BinaryOperator::DivideInteger
-                    | BinaryOperator::Modulo
-                    | BinaryOperator::ModuloInteger,
+                op: BinaryOperator::Multiply | BinaryOperator::Divide | BinaryOperator::Modulo,
                 ..
             } => 2,
             Term::BinaryOperation {
