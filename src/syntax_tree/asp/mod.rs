01b577388063d61376079f55b281de52385de8ff
ZachJHansen <zachhansen@unomaha.edu>
2026-08-26 10:59:06 -0600

    making definite unsupported for conditional literals

--- src/syntax_tree/asp/mod.rs
+++ src/syntax_tree/asp/mod.rs
@@ -6,51 +6,47 @@ pub trait Definite {
     fn definite(&self) -> bool;
 }
 
-impl Definite for mini_gringo_cl::AtomicFormula {
+impl Definite for mini_gringo::AtomicFormula {
     fn definite(&self) -> bool {
         match self {
-            mini_gringo_cl::AtomicFormula::Literal(literal) => {
-                matches!(literal.sign, mini_gringo_cl::Sign::NoSign)
+            mini_gringo::AtomicFormula::Literal(literal) => {
+                matches!(literal.sign, mini_gringo::Sign::NoSign)
             }
-            mini_gringo_cl::AtomicFormula::Comparison(_) => true,
+            mini_gringo::AtomicFormula::Comparison(_) => true,
         }
     }
 }
 
 // TODO : is 'definite' i.e. absence of negation, appropriate for conditional literals?
-impl Definite for mini_gringo_cl::ConditionalLiteral {
-    fn definite(&self) -> bool {
-        let definite_head = match &self.head {
-            mini_gringo_cl::ConditionalHead::AtomicFormula(f) => f.definite(),
-            mini_gringo_cl::ConditionalHead::Falsity => false,
-        };
+// impl Definite for mini_gringo_cl::ConditionalLiteral {
+//     fn definite(&self) -> bool {
+//         let definite_head = match &self.head {
+//             mini_gringo_cl::ConditionalHead::AtomicFormula(f) => f.definite(),
+//             mini_gringo_cl::ConditionalHead::Falsity => false,
+//         };
 
-        let definite_body = {
-            let mut flag = true;
-            for f in self.conditions.formulas.iter() {
-                if !f.definite() {
-                    flag = false;
-                }
-            }
-            flag
-        };
+//         let definite_body = {
+//             let mut flag = true;
+//             for f in self.conditions.formulas.iter() {
+//                 if !f.definite() {
+//                     flag = false;
+//                 }
+//             }
+//             flag
+//         };
 
-        definite_head && definite_body
-    }
-}
+//         definite_head && definite_body
+//     }
+// }
 
-impl Definite for mini_gringo_cl::Rule {
+impl Definite for mini_gringo::Rule {
     fn definite(&self) -> bool {
         match self.head {
-            mini_gringo_cl::Head::Choice(_) => false,
-            mini_gringo_cl::Head::Basic(_) | mini_gringo_cl::Head::Falsity => {
+            mini_gringo::Head::Choice(_) => false,
+            mini_gringo::Head::Basic(_) | mini_gringo::Head::Falsity => {
                 let mut flag = true;
-                for literal in self.body.formulas.iter() {
-                    let cl = match literal {
-                        mini_gringo_cl::BodyLiteral::GfiveConditionalLiteral(cl) => cl,
-                        mini_gringo_cl::BodyLiteral::GsixConditionalLiteral(cl) => cl,
-                    };
-                    if !cl.definite() {
+                for formula in self.body.formulas.iter() {
+                    if !formula.definite() {
                         flag = false;
                     }
                 }
@@ -60,7 +56,7 @@ impl Definite for mini_gringo_cl::Rule {
     }
 }
 
-impl Definite for mini_gringo_cl::Program {
+impl Definite for mini_gringo::Program {
     fn definite(&self) -> bool {
         for rule in self.rules.iter() {
             if !rule.definite() {
@@ -71,14 +67,16 @@ impl Definite for mini_gringo_cl::Program {
     }
 }
 
-impl Definite for gringo::Program {
+impl Definite for mini_gringo_cl::Program {
     fn definite(&self) -> bool {
-        let program: mini_gringo_cl::Program = self.clone().into();
-        program.definite()
+        match mini_gringo::Program::try_from(self.clone()) {
+            Ok(p) => p.definite(),
+            Err(_) => false,
+        }
     }
 }
 
-impl Definite for mini_gringo::Program {
+impl Definite for gringo::Program {
     fn definite(&self) -> bool {
         let program: mini_gringo_cl::Program = self.clone().into();
         program.definite()
@@ -87,17 +85,16 @@ impl Definite for mini_gringo::Program {
 
 #[cfg(test)]
 mod tests {
-    use crate::syntax_tree::asp::{Definite, mini_gringo_cl};
+    use crate::syntax_tree::asp::{Definite, gringo};
 
     #[test]
     fn test_definite_program() {
         for src in [
             "p :- q. q :- p.",
             "p(X) :- q(X), X < Y.",
-            "p(X) :- q(X) : r(X,Y).",
             ":- X = 1..3, nottingham(X). p.",
         ] {
-            let program: mini_gringo_cl::Program = src.parse().unwrap();
+            let program: gringo::Program = src.parse().unwrap();
             assert!(program.definite())
         }
 
@@ -106,9 +103,10 @@ mod tests {
             "p(X) :- not not q(X), X < Y.",
             "p(X) :- #false : q(X).",
             "p(X) :- X = Y : not q(X).",
+            "p(X) :- q(X) : r(X,Y).",
             ":- X = 1..3, not ham(X). p.",
         ] {
-            let program: mini_gringo_cl::Program = src.parse().unwrap();
+            let program: gringo::Program = src.parse().unwrap();
             assert!(!program.definite())
         }
     }
